560733169099e69e836c120f537e9e4c0f95811e
RainSun7 <liangjihang0627@163.com>
2024-04-10 20:58:02 +0800

    Modified the details

--- Assets/Hao Liang/Scripts/Move.cs
+++ Assets/Hao Liang/Scripts/Move.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using TMPro;
 
 public class Move : MonoBehaviour
 {
+ // Rigidbody of the player.
+ private Rigidbody rb;
 
-    private Rigidbody rb;
-    private int count;
-    public float moveSpeed = 5f;
+ // Variable to keep track of collected "PickUp" objects.
+ private int count;
 
-    void Update()
+ public float moveSpeed = 5f;
+ public AudioSource AduioSource;
+
+
+
+void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
@@ -26,45 +33,59 @@ public class Move : MonoBehaviour
         transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.Self);
     }
 
-    public TextMeshProUGUI countText;
+ // UI text component to display count of "PickUp" objects collected.
+ public TextMeshProUGUI countText;
 
-    public GameObject winTextObject;
+ // UI object to display winning text.
+ public GameObject winTextObject;
 
-    void Start()
-       {
+ // Start is called before the first frame update.
+ void Start()
+    {
+ // Get and store the Rigidbody component attached to the player.
         rb = GetComponent<Rigidbody>();
 
+ // Initialize count to zero.
         count = 0;
 
+ // Update the count display.
         SetCountText();
 
+ // Initially set the win text to be inactive.
         winTextObject.SetActive(false);
     }
 
-    void OnTriggerEnter(Collider other)
-        {
-     // Check if the object the player collided with has the "PickUp" tag.
-     if (other.gameObject.CompareTag("PickUp"))
-            {
-     // Deactivate the collided object (making it disappear).
-                other.gameObject.SetActive(false);
+ // This function is called when a move input is detected.
 
-                count = count + 1;
 
-                SetCountText();
-
-            }    
-        }
-        void SetCountText()
+ void OnTriggerEnter(Collider other)
     {
+ // Check if the object the player collided with has the "PickUp" tag.
+ if (other.gameObject.CompareTag("PickUp"))
+        {
+ // Deactivate the collided object (making it disappear).
+            AduioSource.Play();
+            other.gameObject.SetActive(false);
+
+ // Increment the count of "PickUp" objects collected.
+            count = count + 1;
+
+ // Update the count display.
+            SetCountText();
+        }
+    }
+
+ // Function to update the displayed count of "PickUp" objects collected.
+ void SetCountText()
+    {
+ // Update the count text with the current count.
         countText.text = "Count: " + count.ToString();
 
+ // Check if the count has reached or exceeded the win condition.
  if (count >= 5)
         {
+ // Display the win text.
             winTextObject.SetActive(true);
         }
     }
 }
-
-
-
